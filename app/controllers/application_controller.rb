45b7c1db602af2a531e7e1c9ff69bca563ee68ba
Nicola <colao.799211@studenti.uniroma1.it>
2020-06-20 19:13:08 +0200

    avatat pronto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,21 @@ class ApplicationController < ActionController::Base
 	
 	before_action :authenticate_user!
 	
-	def after_sign_in_path_for(resource)
+        before_action :configure_permitted_parameters,
+	 if: :devise_controller?
+          
+          protected
+
+          def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+
+          end
+
+
+
+
+        def after_sign_in_path_for(resource)
 		if resource.class == User
 			if resource.admin_role?
 				rails_admin_path
@@ -14,4 +28,6 @@ class ApplicationController < ActionController::Base
 		end
 	end
 	
+        
+
 end
