b2ae3b38b901b046a9d634616ec779d67e6866e2
andrea-prodon <salsedine98@gmail.com>
2020-06-23 18:59:31 +0200

    aggiunti campi signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
           protected
 
           def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :email, :password, :password_confirmation, :nome, :cognome, :datanascita, :sesso, :peso, :altezza, :indirizzo,  :varie])
             devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
 
           end
