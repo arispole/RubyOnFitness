c07424639600813cc415f1fbf5792bdcd8229be4
andrea-prodon <salsedine98@gmail.com>
2020-06-24 10:48:55 +0200

    FLASH RISOLTI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,26 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'toastr'
 import "../stylesheets/application"
 global.toastr = require("toastr")
+
+$(document).ready(function() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+});
 
 require("@rails/ujs").start()
 require("turbolinks").start()
