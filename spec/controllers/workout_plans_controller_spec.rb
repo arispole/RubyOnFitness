df22cf0b9af1a43254359bf0af8068a60939b3cf
andrea-prodon <salsedine98@gmail.com>
2020-06-25 14:35:11 +0200

    tests

--- spec/controllers/workout_plans_controller_spec.rb
+++ spec/controllers/workout_plans_controller_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "WorkoutPlans", type: :controller do
 
+    render_views
     context "workout plan page rendering test" do
         
         it "should render workout plans template" do
             user = User.create!(nome: 'test', email: 'user@test.com', password: 'password', password_confirmation: 'password')
             sign_in user
             redirect_to  :workout_plans
-            expect(response).to 
+            expect(response).to have_http_status(:success)
         end
       end
 
